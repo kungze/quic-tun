8fb767ae234557b53cd6b6898145c0d13dd325a6
caotingv <1530692952@qq.com>
2022-09-24 17:02:47 +0800

    Add data traffic statistics code

--- pkg/tunnel/tunnel.go
+++ pkg/tunnel/tunnel.go
@@ -2,6 +2,7 @@ package tunnel
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io"
 	"net"
@@ -26,6 +27,8 @@ type tunnel struct {
 	ServerAppAddr      string           `json:"serverAppAddr,omitempty"`
 	RemoteEndpointAddr string           `json:"remoteEndpointAddr"`
 	CreatedAt          string           `json:"createdAt"`
+	SendTotalFlow      int64            `json:"sendTotalFlow"`
+	RecvTotalFlow      int64            `json:"recvTotalFlow"`
 	Protocol           string           `json:"protocol"`
 	ProtocolProperties any              `json:"protocolProperties"`
 	// Used to cache the header data from QUIC stream
@@ -48,10 +51,14 @@ func (t *tunnel) Establish(ctx context.Context) {
 	logger := log.FromContext(ctx)
 	var wg sync.WaitGroup
 	wg.Add(2)
-	t.fillProperties(ctx)
+	var (
+		sendFlowChan = make(chan int64, 1024)
+		recvFlowChan = make(chan int64, 1024)
+	)
+	t.fillProperties(ctx, sendFlowChan, recvFlowChan)
 	DataStore.Store(t.Uuid, *t)
-	go t.conn2Stream(logger, &wg)
-	go t.stream2Conn(logger, &wg)
+	go t.conn2Stream(logger, &wg, sendFlowChan)
+	go t.stream2Conn(logger, &wg, recvFlowChan)
 	logger.Info("Tunnel established successful")
 	// If the tunnel already prepare to close but the analyze
 	// process still is running, we need to cancle it by concle context.
@@ -107,7 +114,7 @@ func (t *tunnel) analyze(ctx context.Context) {
 	}
 }
 
-func (t *tunnel) fillProperties(ctx context.Context) {
+func (t *tunnel) fillProperties(ctx context.Context, sendFlowChan chan int64, recvFlowChan chan int64) {
 	t.StreamID = (*t.Stream).StreamID()
 	if t.Endpoint == constants.ClientEndpoint {
 		t.ClientAppAddr = (*t.Conn).RemoteAddr().String()
@@ -117,9 +124,20 @@ func (t *tunnel) fillProperties(ctx context.Context) {
 	}
 	t.RemoteEndpointAddr = fmt.Sprint(ctx.Value(constants.CtxRemoteEndpointAddr))
 	t.CreatedAt = time.Now().String()
+	go func() {
+		for {
+			select {
+			case <-sendFlowChan:
+				t.SendTotalFlow = int64(<-sendFlowChan)
+			case <-recvFlowChan:
+				t.RecvTotalFlow = int64(<-recvFlowChan)
+			}
+			DataStore.Store(t.Uuid, *t)
+		}
+	}()
 }
 
-func (t *tunnel) stream2Conn(logger log.Logger, wg *sync.WaitGroup) {
+func (t *tunnel) stream2Conn(logger log.Logger, wg *sync.WaitGroup, recvFlowChan chan int64) {
 	defer func() {
 		(*t.Stream).Close()
 		(*t.Conn).Close()
@@ -128,14 +146,14 @@ func (t *tunnel) stream2Conn(logger log.Logger, wg *sync.WaitGroup) {
 	// Cache the first 1024 byte datas, quic-tun will use them to analy the traffic's protocol
 	_, err := io.CopyN(io.MultiWriter(*t.Conn, t.streamCache), *t.Stream, classifier.HeaderLength)
 	if err == nil {
-		_, err = io.Copy(*t.Conn, *t.Stream)
+		_, err = copyBuffer(*t.Conn, *t.Stream, nil, recvFlowChan)
 	}
 	if err != nil {
 		logger.Errorw("Can not forward packet from QUIC stream to TCP/UNIX socket", "error", err.Error())
 	}
 }
 
-func (t *tunnel) conn2Stream(logger log.Logger, wg *sync.WaitGroup) {
+func (t *tunnel) conn2Stream(logger log.Logger, wg *sync.WaitGroup, sendFlowChan chan int64) {
 	defer func() {
 		(*t.Stream).Close()
 		(*t.Conn).Close()
@@ -144,7 +162,7 @@ func (t *tunnel) conn2Stream(logger log.Logger, wg *sync.WaitGroup) {
 	// Cache the first 1024 byte datas, quic-tun will use them to analy the traffic's protocol
 	_, err := io.CopyN(io.MultiWriter(*t.Stream, t.connCache), *t.Conn, classifier.HeaderLength)
 	if err == nil {
-		_, err = io.Copy(*t.Stream, *t.Conn)
+		_, err = copyBuffer(*t.Stream, *t.Conn, nil, sendFlowChan)
 	}
 	if err != nil {
 		logger.Errorw("Can not forward packet from TCP/UNIX socket to QUIC stream", "error", err.Error())
@@ -163,4 +181,47 @@ func NewTunnel(stream *quic.Stream, endpoint string) tunnel {
 		streamCache: &streamCache,
 		connCache:   &connCache,
 	}
+}
+
+func copyBuffer(dst io.Writer, src io.Reader, buf []byte, FlowChan chan int64) (written int64, err error) {
+	if buf == nil {
+		size := 32 * 1024
+		if l, ok := src.(*io.LimitedReader); ok && int64(size) > l.N {
+			if l.N < 1 {
+				size = 1
+			} else {
+				size = int(l.N)
+			}
+		}
+		buf = make([]byte, size)
+	}
+	for {
+		nr, er := src.Read(buf)
+		if nr > 0 {
+			nw, ew := dst.Write(buf[0:nr])
+			if nw < 0 || nr < nw {
+				nw = 0
+				if ew == nil {
+					ew = errors.New("invalid write result")
+				}
+			}
+			written += int64(nw)
+			FlowChan <- written
+			if ew != nil {
+				err = ew
+				break
+			}
+			if nr != nw {
+				err = io.ErrShortWrite
+				break
+			}
+		}
+		if er != nil {
+			if er != io.EOF {
+				err = er
+			}
+			break
+		}
+	}
+	return written, err
 }
